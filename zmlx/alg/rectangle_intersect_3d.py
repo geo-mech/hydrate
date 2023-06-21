bbacd67bf089a4bd8636f92cb7aba04c04232ec5
xutao <xutaotext@163.com>
2023-06-21 17:11:33 +0800

    修改后测试仍不不符合，相交判定数少将近一半

--- zmlx/alg/rectangle_intersect_3d.py
+++ zmlx/alg/rectangle_intersect_3d.py
@@ -1,9 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# print控制
+print_enable = False
 
-# 根据中心点和两条邻边中点坐标获取四个顶点坐标：右上开始，顺指针输出坐标
+
 def calculate_rectangle_vertices(center, midpoint1, midpoint2):
+    """
+    获取矩形顶点坐标
+
+    input: 中心点，邻边1中点坐标，邻边2中点坐标
+
+    return: 四个顶点坐标：右上开始，顺指针输出坐标
+    """
     C = np.array(center)
     M1 = np.array(midpoint1)
     M2 = np.array(midpoint2)
@@ -19,8 +28,14 @@ def calculate_rectangle_vertices(center, midpoint1, midpoint2):
     return vertices
 
 
-# 如果距离大于两个矩形对角线长度的和的一半，那么可以判定两个矩形一定不会相交
 def calculate_max_centers_distance(vertices1, vertices2):
+    """
+    计算两个矩形对角线长度和的一半，判定两个矩形一定不会相交
+
+    input: 矩形1顶点坐标，矩形2顶点坐标
+
+    return： 两个矩形对角线长度和一半
+    """
     # 矩形1对角线长度
     length1 = np.linalg.norm(vertices1[0] - vertices1[2])
 
@@ -30,34 +45,19 @@ def calculate_max_centers_distance(vertices1, vertices2):
     return (length1 + length2) / 2
 
 
-# 判断矩形1的四个点是否在矩形2平面的同一侧
-def judge_same_side(vertices1, vertices2):
-    # 获取平面内一个点
-    point_on_plane = vertices2[0]
-
-    # 计算平面法向量
-    vector1 = vertices2[0] - vertices2[1]
-    vector2 = vertices2[0] - vertices2[2]
-    normal_vector = np.cross(vector1, vector2)
-
-    # 判断是否同一侧
-    same_side = True
-    for point in vertices1:
-        vector = point - point_on_plane
-        dot_product = np.dot(vector, normal_vector)
-        if dot_product < 0:
-            same_side = False
-            break
-
-    return same_side
-
-
-# 计算变换矩阵和逆变换矩阵
 def calculate_transform_matrix(vertices):
+    """
+    计算变换矩阵和逆变换矩阵
+
+    input: 矩形2坐标
+
+    return：变换矩阵和逆变换矩阵
+    """
     # 获取矩形2的中心点和邻边中点坐标
     center2 = (vertices[0] + vertices[2]) / 2
     mid1 = (vertices[1] - vertices[2]) / 2
     mid2 = (vertices[3] - vertices[2]) / 2
+
     # 获取矩形2局部坐标系的xyz单位向量
     vector_x = mid1 - center2
     vector_y = mid2 - center2
@@ -75,8 +75,14 @@ def calculate_transform_matrix(vertices):
     return transform_matrix, inverse_transform_matrix
 
 
-# 找出不同符号的数的索引
 def find_different_sign_index(arr):
+    """
+    找出不同符号的数的索引
+
+    input: np.array()
+
+    return: 是否存在不同符号的数，存在返回其索引
+    """
     signs = np.sign(arr)
     unique_signs = np.unique(signs)
 
@@ -90,22 +96,49 @@ def find_different_sign_index(arr):
     return different_sign_index
 
 
-# 获得矩形1顶点的在矩形2的两侧分布情况
 def get_points_distribution(local_z):
+    """
+    获得矩形1顶点的在矩形2的两侧分布情况
+
+    input: 局部坐标系下的矩形四个顶点z坐标
+
+    return: 返回点的分布情况及对应的索引（字典）
+    """
     #获得四个z坐标的，检查四个数的符号
+    assert len(local_z) == 4
+
     product = np.prod(local_z)
-    if product > 0:
-        print("两侧两点")
-        return None
-    else:
-        # 找出不同符号的数的索引
+    signs = np.sign(local_z)
+    unique_signs = np.unique(signs)
+    if product > 0 and len(unique_signs) == 1:
+        # 一侧四点
+        return {'no': None}
+
+    elif product > 0 and len(unique_signs) == 2:
+        # 两侧两点
+        positive_indices = np.where(np.array(local_z) > 0)[0]
+        negative_indices = np.where(np.array(local_z) < 0)[0]
+        return {'22': np.hstack((positive_indices, negative_indices))}
+
+    elif product < 0 and len(unique_signs) == 2:
+        # 一侧一点，一侧三点
         negative_index = find_different_sign_index(local_z)
-        print(f"一侧一点，一侧三点，一点索引为{negative_index}")
-        return negative_index
+        return {'13': negative_index}
+
+    elif product == 0:
+        # 点在面上
+        zero_indices = np.where(np.array(local_z) == 0)[0]
+        return {'inside': zero_indices}
 
 
-# 计算线段与局部坐标系X-O-Y的交点
 def calculate_intersection_point(line_begin, line_end):
+    """
+    计算线段与局部坐标系X-O-Y的交点
+
+    input：线段起始点坐标， 线段结束点坐标
+
+    return：线段与局部坐标系的交点
+    """
     # z = 0, 利用向量共线计算交点x,y坐标
     x = (line_begin[0] * line_end[2] -
          line_begin[2] * line_end[0]) / (line_end[2] - line_begin[2])
@@ -114,8 +147,14 @@ def calculate_intersection_point(line_begin, line_end):
     return np.array([x, y])
 
 
-# 判断点是否在矩形内部
 def point_in_rectangle(point, rectangle):
+    """
+    判断二维平面中点是否在矩形内部
+
+    input：点坐标，矩形顶点坐标
+
+    return：bool 内部：True；外部：False
+    """
     x, y = point
     x_min, y_min, x_max, y_max = rectangle
 
@@ -125,8 +164,15 @@ def point_in_rectangle(point, rectangle):
         return False
 
 
-# 计算线段与矩形的位置情况
 def line_rect_intersection(p1, p2, rect):
+    """
+    计算2d线段与矩形的位置情况
+
+    input： 线段起点，线段终点，矩形顶点坐标
+
+    return：有交点返回交点，无交点返回None
+
+    """
     # 矩形的四个顶点
     r1, r2, r3, r4 = rect
     x_min = -r1[0]
@@ -153,14 +199,16 @@ def line_rect_intersection(p1, p2, rect):
             return None
     elif len(intersections) == 1:
         # 一个交点
-        return intersections[0]
+        return intersections
     elif len(intersections) == 2:
         # 两个交点
         return intersections
 
 
-# 计算两条线之间的位置情况
 def line_line_intersection(p1, p2, p3, p4):
+    """
+    计算两条线之间的位置情况
+    """
     # 利用numpy求解两条线段的交点
     A = np.array([[p2[0] - p1[0], p3[0] - p4[0]],
                   [p2[1] - p1[1], p3[1] - p4[1]]])
@@ -176,8 +224,10 @@ def line_line_intersection(p1, p2, p3, p4):
         return None
 
 
-# 绘制矩形轮廓
 def plot_rectangle(vertices):
+    """
+    绘制矩形轮廓
+    """
     # 创建3D图形对象
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -202,11 +252,14 @@ def plot_rectangle(vertices):
     plt.show()
 
 
-
 def calculate_3d_rectangle_intersect(center1, edge1_center1, edge2_center1,
                                      center2, edge1_center2, edge2_center2):
     """
     判断三维空间中的两个矩形的相交情况，并计算交线
+
+    input：矩形1中心点，矩形1邻边中点1，矩形1邻边中点1，矩形2中心点，矩形2邻边中点1，矩形2邻边中点1
+
+    return：相交情况，有交线返回交线，无交线返回None，相同平面相互包含返回一个标签？
     """
     # 获取矩形的四个顶点坐标
     rect1_vertices = calculate_rectangle_vertices(center1, edge1_center1,
@@ -222,84 +275,122 @@ def calculate_3d_rectangle_intersect(center1, edge1_center1, edge2_center1,
     distance = np.linalg.norm(np.array(center1) - np.array(center2))
 
     # 定义距离精度
-    e_dist = 0.1
+    e_dist = 0
 
     # 判断是否可能相交
     if distance - max_distance > e_dist:
-        print("两个矩形不可能相交")
+        if print_enable:
+            print("两个矩形不可能相交")
         return None
     else:
+        # 获取变换矩阵及逆变换矩阵 注意：1x2的列表，[变换矩阵,逆变换矩阵]
+        tranform_matrix = calculate_transform_matrix(rect2_vertices)
 
-        # 判断其中一个矩形四个顶点是否在另外一个矩形的同一侧
-        if judge_same_side(rect1_vertices, rect2_vertices):
-            print("四个点均在同侧，不可能相交")
+        # 将全局坐标转换为局部坐标 右上开始，顺指针输出坐标，
+        # 局部坐标定: 矩形2中心点为O，中心点到邻边中心点分别为XY轴，两轴叉乘法向量为Z轴
+        local_coord = [
+            np.dot(tranform_matrix[0], vertex) for vertex in rect1_vertices
+        ]
+
+        # 计算局部坐标系下矩形2的四个顶点坐标：计算长宽即可得到
+        width = np.linalg.norm(
+            np.array(rect2_vertices[0]) - np.array(rect2_vertices[1]))
+        length = np.linalg.norm(
+            np.array(rect2_vertices[1]) - np.array(rect2_vertices[2]))
+        local_rect2_vertices = (
+            np.array([length / 2, width / 2]),
+            np.array([length / 2, -width / 2]),
+            np.array([-length / 2, -width / 2]),
+            np.array([-length / 2, width / 2]),
+        )
+
+        # 判断点分布情况：13分布/22分布 -利用局部坐标系下的local_z的符号判断
+        local_z = [vertex[2] for vertex in local_coord]
+
+        # 获取分布情况  no: None; 22: positive_indices, negative_indices
+        # 13: negative_indices; inside: zero_indices
+        distribution = get_points_distribution(local_z)
+        label = list(distribution.keys())[0]
+        value = list(distribution.values())[0]
+
+        if label == 'no':
+            # 一侧四点 无交点
             return None
-        else:
-            #! 一个点或者两个点在不同侧，计算交线
-            # 获取变换矩阵及逆变换矩阵 注意：1x2的列表，[变换矩阵,逆变换矩阵]
-            tranform_matrix = calculate_transform_matrix(rect2_vertices)
 
-            # 将全局坐标转换为局部坐标 右上开始，顺指针输出坐标，
-            # 局部坐标定: 矩形2中心点为O，中心点到邻边中心点分别为XY轴，两轴叉乘法向量为Z轴
-            local_coord = [
-                np.dot(tranform_matrix[0], vertex) for vertex in rect1_vertices
-            ]
-
-            # 计算局部坐标系下矩形2的四个顶点坐标：计算长宽即可得到
-            width = np.linalg.norm(
-                np.array(rect2_vertices[0]) - np.array(rect2_vertices[1]))
-            length = np.linalg.norm(
-                np.array(rect2_vertices[1]) - np.array(rect2_vertices[2]))
-            local_rect2_vertices = (
-                np.array([length / 2, width / 2]),
-                np.array([length / 2, -width / 2]),
-                np.array([-length / 2, -width / 2]),
-                np.array([-length / 2, width / 2]),
-            )
-
-            # 判断点分布情况：13分布/22分布 -利用局部坐标系下的local_z的符号判断
-            local_z = [vertex[2] for vertex in local_coord]
-            distribution = get_points_distribution(local_z)
-
-            if distribution == None:
-                # 22分布
-                intersec1 = calculate_intersection_point(
-                    local_coord[0], local_coord[1])
-                intersec2 = calculate_intersection_point(
-                    local_coord[2], local_coord[3])
-                print(intersec1, intersec2)
+        elif label == 'inside':
+            # 矩形1顶点在矩形2的平面上
+            if len(value) == 1:
+                # 一个点在相同平面
+                inside = point_in_rectangle(local_coord[value],
+                                            local_rect2_vertices)
+                if inside:
+                    # 该点在矩形内部
+                    return local_coord[value]
+                else:
+                    # 该点在矩形外部
+                    return None
+            elif len(value) == 2:
+                # 两个点在相同平面
+                point1 = local_coord[value[0]]
+                point2 = local_coord[value[1]]
+                return line_rect_intersection(point1, point2,
+                                              local_rect2_vertices)
             else:
-                # 13分布 此时distribution为单独一侧点的索引
-                intersec1 = calculate_intersection_point(
-                    local_coord[distribution], local_coord[distribution + 1])
-                intersec2 = calculate_intersection_point(
-                    local_coord[distribution], local_coord[distribution - 1])
+                #todo 两个矩形在相同平面
+                assert len(value) == 4
+                # 
+                return '相同平面'
 
-            # 交线与X-O-Y平面内的矩形的关系：一个交点，两个交点，无交点（相离，包含）
-            local_intersect_line = line_rect_intersection(
-                intersec1, intersec2, local_rect2_vertices)
+        elif label == '22':
+            # 22分布
+            intersec1 = calculate_intersection_point(local_coord[value[0]],
+                                                     local_coord[value[2]])
+            intersec2 = calculate_intersection_point(local_coord[value[1]],
+                                                     local_coord[value[3]])
 
-            # 判断局部坐标系下是否存在交点
-            if local_intersect_line == None:
-                #不存在交点
-                return None
+        elif label == '13':
+            # 13分布
+            if value == 1 or value == 2:
+                intersec1 = calculate_intersection_point(local_coord[value],
+                                                        local_coord[value+1])
+                intersec2 = calculate_intersection_point(local_coord[value],
+                                                        local_coord[value-1])
+            elif value == 0:
+                intersec1 = calculate_intersection_point(local_coord[0],
+                                                        local_coord[1])
+                intersec2 = calculate_intersection_point(local_coord[0],
+                                                        local_coord[3])
+            else:
+                assert value == 3
+                intersec1 = calculate_intersection_point(local_coord[3],
+                                                        local_coord[0])
+                intersec2 = calculate_intersection_point(local_coord[3],
+                                                        local_coord[2])
+       
+        # 交线与X-O-Y平面内的矩形的关系：一个交点，两个交点，无交点（相离，包含）
+        local_intersect_line = line_rect_intersection(intersec1, intersec2,
+                                                      local_rect2_vertices)
 
-            else:  # 存在交点
-                # 添加z轴，设置为零
-                z_coordinates = np.array([0])
+        # 判断局部坐标系下是否存在交点
+        if local_intersect_line is None:
+            #不存在交点
+            return None
 
-                # 为二维坐标添加z值
-                local_intersect_line_3d = [
-                    np.hstack((vertex, z_coordinates))
-                    for vertex in local_intersect_line
-                ]
+        else:  # 存在交点
+            # 添加z轴，设置为零
+            z_coordinates = np.array([0])
 
-                # 变换回全局坐标
-                global_intersect_line = [
-                    np.dot(tranform_matrix[1], vertex)
-                    for vertex in local_intersect_line_3d
-                ]
-                return global_intersect_line
+            # 为二维坐标添加z值
+            local_intersect_line_3d = [
+                np.hstack((vertex, z_coordinates))
+                for vertex in local_intersect_line
+            ]
+            # 变换回全局坐标
+            global_intersect_line = [
+                np.dot(tranform_matrix[1], vertex)
+                for vertex in local_intersect_line_3d
+            ]
+            return global_intersect_line
 
 
 if __name__ == '__main__':
@@ -316,7 +407,7 @@ if __name__ == '__main__':
 
     # test2
     center1 = (0, 0, 0)
-    edge1_center1 = (0.2, 0, 0)
+    edge1_center1 = (0.8, 0, 0)
     edge2_center1 = (0, 1, 1)
 
     center2 = (0, 0, 0)
