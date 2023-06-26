731c5132699610c6f937527128e409c5e6fd52e0
xutao <xutaotext@163.com>
2023-06-26 22:29:03 +0800

    测试代码更新
    目前计算links与生成links一致
    
    测试代码修改
    
    upload test data
    
    test code modified
    
    fix bug
    
    test code modified
    
    add rect_intersec_3d data

---
+++ zmlx/data/rect_intersec_3d/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from zml import *
+import os
+import numpy as np
+
+# 读取生成的相交矩阵序号
+demo_path = os.path.join(os.path.dirname(__file__), 'demo_links.txt')
+
+# 读取矩形定义坐标
+vertices_path = os.path.join(os.path.dirname(__file__), 'vertices.txt')
+
+# 读取计算得到的相交矩阵序号
+
+result_path = os.path.join(os.path.dirname(__file__), 'cal_links.txt')
+
+
+def get_demo_rect_index():
+    """
+    读取生成的相交矩阵序号
+    """
+    return np.loadtxt(demo_path,dtype=int)
+
+
+def get_demo_rect_vertices():
+    """
+    读取矩形定义坐标
+    """
+    return np.loadtxt(vertices_path)
+
+
+def get_cal_rect_index():
+    """
+    读取计算得到的相交矩阵序号
+    """
+    return np.loadtxt(result_path,dtype=int)
+
+if __name__ == '__main__':
+    print(get_demo_rect_index())
